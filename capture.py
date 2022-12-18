21cb295eb1c3c4b5e3378c2562e2b227bf6f697a
Bryce Nichols <74982626+nicholsbp@users.noreply.github.com>
2022-12-18 15:58:26 -0800

    Create capture.py

---
+++ capture.py
@@ -0,0 +1,26 @@
+import cv2
+import time
+import datetime
+
+# RTSP stream URL
+rtsp_url = 'rtsp://yourRTSPsource.lan/stream_1080'
+
+# OpenCV video capture object
+cap = cv2.VideoCapture(rtsp_url)
+
+while True:
+    # Capture frame
+    ret, frame = cap.read()
+
+    now = datetime.datetime.now()
+
+    # Format date and time for use in filename
+    filename = f"image_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.jpg"
+
+    # Save frame as JPEG image
+    cv2.imwrite(filename, frame)
+
+    # Wait 
+    time.sleep(3600)
+
+cap.release()
